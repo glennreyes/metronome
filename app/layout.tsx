7d570fc67cde933b1e7448ec785bfc381835bb81
Glenn Reyes <glenn@glennreyes.com>
2024-02-26 17:07:08 +0100

    Add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from 'next';
 import type { ReactNode } from 'react';
 
+import { GitHub } from '@/components/icons/github';
+import { X } from '@/components/icons/x';
 import { cn } from '@/lib/utils';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
@@ -32,7 +34,50 @@ const RootLayout = ({ children }: RootLayoutProps) => (
         'dark bg-background font-sans text-foreground motion-safe:scroll-smooth',
       )}
     >
-      {children}
+      <div className="container grid min-h-dvh max-w-screen-lg">
+        {children}
+        <footer className="flex items-center justify-between gap-2">
+          <p className="py-4 text-sm text-muted-foreground">
+            Built by{' '}
+            <a
+              className="underline underline-offset-4"
+              href="https://glennreyes.com"
+              rel="noreferrer"
+              target="_blank"
+            >
+              @glnnrys
+            </a>
+            . Source on{' '}
+            <a
+              className="underline underline-offset-4"
+              href="https://github.com/glennreyes/tuner"
+            >
+              GitHub
+            </a>
+            .
+          </p>
+          <div className="flex gap-2 py-4">
+            <a
+              className="items-center justify-center rounded-full p-2 text-muted-foreground transition-colors hover:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+              href="https://x.com/glnnrys"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <X className="h-5 w-5" />
+              <span className="sr-only">Twitter</span>
+            </a>
+            <a
+              className="items-center justify-center rounded-full p-2 text-muted-foreground transition-colors hover:text-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+              href="https://github.com/glennreyes/tuner"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <GitHub className="h-5 w-5" />
+              <span className="sr-only">Twitter</span>
+            </a>
+          </div>
+        </footer>
+      </div>
       <Analytics />
       <SpeedInsights />
     </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Metronome } from '@/components/metronome';
 
 const RootPage = () => (
-  <main className="container flex min-h-screen items-center justify-center">
+  <main className="container flex items-center justify-center">
     <Metronome />
   </main>
 );
