bca35fc647f36684edb6ff3ddd4dfa80650bc6b3
Glenn Reyes <glenn@glennreyes.com>
2024-02-26 17:03:42 +0100

    Polish

--- components/metronome.tsx
+++ components/metronome.tsx
@@ -1,23 +1,36 @@
 'use client';
 
 import { click } from '@/lib/sfx';
-import { Pause, Play } from 'lucide-react';
-import { type FC, useEffect, useMemo, useState } from 'react';
+import { cn } from '@/lib/utils';
+import { Pause, Play, Zap, ZapOff } from 'lucide-react';
+import { type FC, useCallback, useEffect, useMemo, useState } from 'react';
 
 import { Button } from './ui/button';
+import { Card, CardHeader } from './ui/card';
 import { Slider } from './ui/slider';
 
 export const Metronome: FC = () => {
-  const [bpm, setBpm] = useState(120);
+  const [bpm, setBpm] = useState(90);
   const [playing, setPlaying] = useState(false);
-  const audioContext = useMemo(() => new AudioContext(), []);
+  const [highlight, setHighlight] = useState(false);
+  const [highlightEnabled, setHighlightEnabled] = useState(true);
   const interval = useMemo(() => (60 / bpm) * 1000, [bpm]);
+  const audioContext = useMemo(() => new AudioContext(), []);
+  const beat = useCallback(() => {
+    click(audioContext);
+
+    setHighlight(true);
+    setTimeout(() => {
+      setHighlight(false);
+    }, interval * 0.5);
+  }, [audioContext, interval]);
 
   useEffect(() => {
     if (playing) {
-      click(audioContext);
+      beat();
+
       const intervalId = setInterval(() => {
-        click(audioContext);
+        beat();
       }, interval);
 
       return () => {
@@ -26,10 +39,13 @@ export const Metronome: FC = () => {
     }
 
     return;
-  }, [audioContext, interval, playing]);
+  }, [beat, interval, playing]);
 
   const handlePlayStop = () => {
     setPlaying(!playing);
+  };
+  const handleHighlightChange = () => {
+    setHighlightEnabled(!highlightEnabled);
   };
   const handleTempChange = ([value]: number[]) => {
     if (playing) {
@@ -40,35 +56,57 @@ export const Metronome: FC = () => {
   };
 
   return (
-    <div className="flex w-full flex-col justify-center gap-16 md:w-80">
-      <div className="text-center">
-        <div className="text-8xl font-bold">{bpm}</div>
-        <div className="text-muted-foreground">BPM</div>
-      </div>
+    <Card
+      className={cn({
+        'border-primary/25 shadow-primary/25': highlightEnabled && highlight,
+        'shadow-xl transition duration-300': highlightEnabled,
+      })}
+    >
+      <CardHeader>
+        <div className="flex w-full flex-col justify-center gap-16 md:w-80">
+          <div className="text-center">
+            <div className="text-8xl">{bpm}</div>
+            <div className="text-muted-foreground">BPM</div>
+          </div>
 
-      <div className="flex flex-col justify-center gap-6">
-        <div className="flex flex-col items-center gap-2 md:flex-row">
-          <Slider
-            max={400}
-            min={24}
-            onValueChange={handleTempChange}
-            step={1}
-            value={[bpm]}
-          />
+          <div className="flex flex-col justify-center gap-6">
+            <div className="flex flex-col items-center gap-2 md:flex-row">
+              <Slider
+                max={400}
+                min={24}
+                onValueChange={handleTempChange}
+                step={1}
+                value={[bpm]}
+              />
+            </div>
+            <div className="flex gap-2">
+              <Button
+                className="flex-1"
+                onClick={handlePlayStop}
+                size="icon"
+                variant={playing ? 'secondary' : undefined}
+              >
+                {playing ? (
+                  <Pause className="h-5 w-5" strokeWidth={1.5} />
+                ) : (
+                  <Play className="h-5 w-5" strokeWidth={1.5} />
+                )}
+              </Button>
+              <Button
+                onClick={handleHighlightChange}
+                size="icon"
+                variant="ghost"
+              >
+                {highlightEnabled ? (
+                  <Zap className="h-5 w-5" strokeWidth={1.5} />
+                ) : (
+                  <ZapOff className="h-5 w-5" strokeWidth={1.5} />
+                )}
+              </Button>
+            </div>
+          </div>
         </div>
-        <Button
-          className="w-full"
-          onClick={handlePlayStop}
-          size="icon"
-          variant={playing ? 'secondary' : undefined}
-        >
-          {playing ? (
-            <Pause className="h-5 w-5" strokeWidth={1.5} />
-          ) : (
-            <Play className="h-5 w-5" strokeWidth={1.5} />
-          )}
-        </Button>
-      </div>
-    </div>
+      </CardHeader>
+    </Card>
   );
 };
